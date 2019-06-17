090bbe826e61b41d858574f544c354acb953c7ce
Onni Heinonen <onni.heinonen98@gmail.com>
2019-06-17 13:43:47 +0300

    Controllers for character and camera

---
+++ 03_sidescroller/Assets/cameraController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject background;
+    public float camDistance;
+    float camSpeed;
+    public float minCamSpeed = 0.3f;
+    public float maxCamSpeed = 0.7f;
+    float maxDistance = 0;
+    bool inPos = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string playerOrientation = GameObject.Find("Player").GetComponent<characterController>().orientation;
+
+        Vector2 playerPos = player.transform.position;
+        Vector2 newCamPos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 bgMin = background.GetComponent<BoxCollider2D>().bounds.min;
+        Vector2 bgMax = background.GetComponent<BoxCollider2D>().bounds.max;
+
+        float maxX = bgMax.x;
+        float minX = bgMin.x;
+        float maxY = bgMax.y;
+        float minY = bgMin.y;
+
+        if (playerPos.x < maxX && playerPos.x > minX)
+        {
+            newCamPos.x = playerPos.x;
+        }
+
+        if (playerPos.y < maxY && playerPos.y > minY)
+        {
+            newCamPos.y = playerPos.y;
+        }
+
+        if(playerOrientation == "Right")
+        {
+            newCamPos.x += camDistance;
+        }
+        else
+        {
+            newCamPos.x -= camDistance;
+        }
+
+        Vector3 target = new Vector3(newCamPos.x, newCamPos.y, transform.position.z);
+        float distance = Mathf.Abs(transform.position.x - target.x);
+
+        if (distance > maxDistance)
+        {
+            maxDistance = distance;
+        }
+
+        camSpeed = distance / maxDistance / 10;
+
+        Debug.Log(distance + " " + inPos);
+
+        if (distance > 0.3)
+        {
+            inPos = false;
+        }
+        else
+        {
+            inPos = true;
+        }
+
+        if(!inPos)
+        {
+            transform.position = Vector3.Lerp(transform.position, target, Mathf.Clamp(camSpeed, minCamSpeed, maxCamSpeed));
+        }
+        else
+        {
+            transform.position = target;
+        }
+    }
+}
---
+++ 03_sidescroller/Assets/characterController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+    public GameObject player;
+    public float maxSpeed;
+    public float smoothing = 5;
+    public float jumpForce = 2;
+    public string orientation = "Right";
+    float speed = 0;
+
+    private void Move(string str)
+    {
+        float horizontal = 0;
+
+        switch (str)
+        {
+            case "Right":
+                horizontal = 1;
+                break;
+            case "Left":
+                horizontal = -1;
+                break;
+        }
+
+        if(speed < maxSpeed)
+        {
+            speed += smoothing;
+        }
+
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2(horizontal * speed * Time.fixedDeltaTime, player.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    private void Jump()
+    {
+        Vector2 playerVelocity = player.GetComponent<Rigidbody2D>().velocity;
+
+        if (playerVelocity.y == 0)
+        {
+            player.GetComponent<Rigidbody2D>().velocity = new Vector2(playerVelocity.x, jumpForce);
+        }
+    }
+
+    private void Flip(string str)
+    {
+        if (str == "Right")
+        {
+            Vector3 temp = transform.localScale;
+            temp.x *= -1;
+            transform.localScale = temp;
+        }
+        else if (str == "Left")
+        {
+            Vector3 temp = transform.localScale;
+            temp.x = Mathf.Abs(temp.x);
+            transform.localScale = temp;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        if(horizontal > 0)
+        {
+            Move("Right");
+            Flip("Right");
+            orientation = "Right";
+        }
+        else if (horizontal < 0)
+        {
+            Move("Left");
+            Flip("Left");
+            orientation = "Left";
+        }
+        else
+        {
+            Vector2 playerVelocity = player.GetComponent<Rigidbody2D>().velocity;
+
+            speed = 0;
+
+            if (playerVelocity.x > 0.1)
+            {
+                playerVelocity.x -= smoothing * Time.fixedDeltaTime;
+            }
+            else if (playerVelocity.x < -0.1)
+            {
+                playerVelocity.x += smoothing * Time.fixedDeltaTime;
+            }
+            else
+            {
+                playerVelocity.x = 0;
+            }
+
+            player.GetComponent<Rigidbody2D>().velocity = playerVelocity;
+        }
+    }
+}
